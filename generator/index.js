753280ece273db28c7d4c5ed2efb81adbf663cd8
Gopal <mgopalgk@gmail.com>
2024-02-28 20:23:45 +0530

    gsheet based code generation added

--- generator/index.js
+++ generator/index.js
@@ -1,4 +1,5 @@
 module.exports = function (plop) {
     plop.setGenerator('component', require('./component/index.js'));
     plop.setGenerator('page', require('./page/index.js'));
+    plop.setGenerator('list', require('./list/index.js'));
 };
---
+++ src/components/Users/index.js
@@ -0,0 +1,13 @@
+<ul>
+    <li>Name: New, Age: Header</li>
+    <li>Name: New, Age: Header</li>
+    <li>Name: New, Age: Header</li>
+    <li>Name: New, Age: Header</li>
+    <li>Name: New, Age: LineItem</li>
+    <li>Name: New, Age: LineItem</li>
+    <li>Name: New, Age: LineItem</li>
+    <li>Name: New, Age: LineItem</li>
+    <li>Name: New, Age: LineItem</li>
+    <li>Name: New, Age: LineItem</li>
+    <li>Name: New, Age: LineItem</li>
+</ul>
